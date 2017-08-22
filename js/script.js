4e5f56e4b71cf4f2fe2109bddec6215628894690
Tim Stannard <stannard.tim@gmail.com>
2017-08-23 11:34:06 +1200

    test

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var map, marker, infobox;
+var markers = [];
+var markerLocations = [ //array of restaurants
+  {
+    lat: -41.2943592,
+    lng: 174.7753067,
+    title: "Loretta Cafe and Restaurant",
+  },
+  {
+    lat: -41.294281,
+    lng: 174.784976,
+    title: "Five Boroughs Deli/Diner",
+  },
+  {
+    lat: -41.2942663,
+    lng: 174.7832352,
+    title: "Sweet Mothers Kitchen",
+  },
+    {
+    lat: -41.2924698,
+    lng: 174.7793674,
+    title: "Enigma Cafe",
+  }
+];
